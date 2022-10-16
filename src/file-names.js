4aee2ded63b1437de2379ce61600cb78f69a66c5
222 <kiravishn9@gmail.com>
2022-10-16 09:25:49 -0400

    done

--- src/file-names.js
+++ src/file-names.js
@@ -16,8 +16,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  function renameFiles(names) {
+    for (let i in names) { 
+      let j = 1;
+      if (names.slice(0,i).includes(names[i])){ 
+        if (names.slice(0,i).includes(names[i]+"("+j.toString()+")")){ 
+          j++;
+        }
+        names[i] += "(" + j.toString() + ")";
+      }
+    }
+    return names;
+  }
 }
 
 module.exports = {
